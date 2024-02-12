45ffc7f6dd8ab4f174489a9e44222ed9eb88b663
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2024-02-12 23:52:07 +0100

    Adding pics and 2 components

---
+++ vite-project/src/lib/DescripcionPerfiles.svelte
@@ -0,0 +1,53 @@
+<script>
+    let src = '/img/basadur.png';    
+</script>
+
+<main>
+    <section>
+        <div class="Basadur">
+            <h1>Perfil Basadur</h1>
+            <p>para conocer tu perfil innovador <br> y tu funcion en el equipo</p>
+        </div>
+        <img {src} alt="basadur-foto">
+    </section>
+    
+    <section>
+        <div class="Personalidad">
+            <h1>Perfil de Personalidad</h1>
+            <p>para mejorar la empatía y <br> la comunicación hacia los demás</p>
+        </div>
+        <img src="/img/personalidad.png" alt="personalidad-foto">
+    </section>
+   
+</main>
+
+<style>
+    main{
+        padding: 20px;
+    }
+    section{
+        display: flex;
+        justify-content: space-around;
+        margin-top: 10px;
+    }
+
+    .Basadur{
+        margin-right: 98px;
+    }
+
+    h1{
+        font-family: sansation;
+        font-size: xx-large;
+        font-weight: bold;   
+    }
+
+    p{
+        font-family: sansation;
+        font-weight: 100;
+    }
+
+    img{
+        width: 19%;
+    }
+
+</style>
---
+++ vite-project/src/lib/PorqueLosTest.svelte
@@ -0,0 +1,47 @@
+<main>
+    <section class="pregunta">
+        <div>
+            <h1>¿POR QUÉ HACER LOS TESTS</h1>
+        </div>
+    </section>
+
+    <section class="respuestas">
+        <div class="porque1">
+            <img src="/img/Settings.png" alt="rueda">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, a dignissimos rerum labore ex quaerat enim voluptates quasi ab dolorum minus dicta quibusdam vero exercitationem accusamus ut pariatur necessitatibus aperiam.</p>
+        </div>
+
+        <div class="porque2">
+            <img src="/img/Meeting Room.png" alt="mesa">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quibusdam blanditiis consequatur corporis libero nisi doloremque expedita mollitia repellat itaque veritatis accusantium dicta aliquid tempore temporibus placeat excepturi, consectetur sint.</p>
+        </div>
+       
+        <div class="porque3">
+            <img src="/img/Onboarding.png" alt="ayuda">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio fuga aliquid soluta quos quibusdam amet tempore, reiciendis necessitatibus iste vero iusto nemo ullam? Est, beatae adipisci! Quisquam sed facilis officia!</p>
+        </div>
+    </section>
+</main>
+
+<style>
+    main{
+        background-color: white;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        font-family: sansation;
+        padding: 20px
+    }
+
+    img{
+        width: 50px;
+    }
+    
+    .respuestas{
+        display: block; 
+    }
+
+    .porque2{
+        padding-left: 276px
+    }
+</style>
