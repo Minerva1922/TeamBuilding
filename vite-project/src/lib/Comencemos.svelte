c250eb8c494faaa64b104f57b4e766e4b8201122
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2024-02-27 22:02:01 +0100

    Giving styles to badasur profiles and personality tests

--- vite-project/src/lib/Comencemos.svelte
+++ vite-project/src/lib/Comencemos.svelte
@@ -2,14 +2,14 @@
     <h1>Comencemos...</h1>
     <section>
         <div class="PerfilBasadur">
-            <h2>Perfil Basadur</h2>
+            <h2>Perfil <br> Basadur</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, distinctio alias culpa perspiciatis suscipit dolorum totam? Officiis expedita aspernatur nisi atque illum alias architecto natus, vel, autem corporis corrupti deserunt.</p>
-            <button>Más Detalles</button>
+            <button>Haz el test</button>
         </div>
         <div class="PerfilPersonalidad">
             <h2>Perfil de Personalidad</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore deserunt nam velit? Rerum, ab qui nulla ut quidem reiciendis non libero quo accusantium obcaecati labore, ullam minima provident temporibus excepturi!</p>
-            <button>Más Detalles</button>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, distinctio alias culpa perspiciatis suscipit dolorum totam? Officiis expedita aspernatur nisi atque illum alias architecto natus, vel, autem corporis corrupti deserunt.</p>
+            <button>Haz el test</button>
         </div>
     </section>
 </main>
@@ -18,21 +18,42 @@
     main{
         font-family: sansation;
     }
+
     h1{
         text-align: center;
     }
+
+    h2{
+        text-align: center;
+        font-size: xxx-large;
+    }
     
+    p {
+        margin: 30px;
+        text-align: justify;
+    }
+
+    button{
+        width: 242px;
+        height: 60px;
+        border-radius: 8px;
+    }
+
     section{
         display: flex;
-        padding: 30px;
-        text-align: center;
+        flex-direction: row;
+        justify-content: space-around;
     }
 
     .PerfilBasadur{
         background-color: white;
+        width: 408px;
+        height: 704px;
     }
 
     .PerfilPersonalidad{
         background-color: white;
+        width: 408px;
+        height: 704px;
     }
 </style>
